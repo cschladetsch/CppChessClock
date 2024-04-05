8f9df055267b5c8b4633f027fac386b9978b8af4
Christian <christian.schladetsch@gmail.com>
2024-04-06 03:48:28 +1100

    Update 2024

--- Building.md
+++ Building.md
@@ -10,19 +10,19 @@ TODO
 
 
 ```bash
-sudo apt-get update 
-sudo apt-get install build-essential git-lfs libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev uuid-dev
-sudo apt-get install libboost-chrono.dev libboost-filesystem.dev libboost-program-options.dev libboost-system.dev libboost-date-time-dev 
-
-
-git clone https://github.com/cschladetsch/ChessClock.git
-cd ChessClock
-git lfs init
-git fetch origin develop
-git checkout develop
-git submodule init
-git submodule update --recursive
-
+# sudo apt-get update 
+# sudo apt-get install build-essential git-lfs libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev uuid-dev
+# sudo apt-get install libboost-chrono.dev libboost-filesystem.dev libboost-program-options.dev libboost-system.dev libboost-date-time-dev 
+# 
+# 
+# git clone https://github.com/cschladetsch/ChessClock.git
+# cd ChessClock
+# git lfs init
+# git fetch origin develop
+# git checkout develop
+# git submodule init
+# git submodule update --recursive
+# 
 ```
 
 # Building
--- CMake/AddBoost.cmake
+++ CMake/AddBoost.cmake
@@ -1,27 +1,17 @@
 #set(Boost_DEBUG 1)
 
-message(STATUS "Looking for boost")
-
-if (WIN32)
-	set(Boost_NO_SYSTEM_PATHS on CACHE BOOL "Do not search system for Boost")
-	set(BOOST_ROOT "c:/boost/boost_1_75_0" CACHE PATH "Boost library path")
-	set(Boost_INCLUDE_DIR ${CMAKE_INCLUDE_PATH} "c:/boost/boost_1_75_0")
-	set(Boost_LIBRARY_DIR ${CMAKE_LIBRARY_PATH} "c:/boost/boost_1_75_0/stage/lib")
-endif()
-
+set(BOOST_ROOT "c:/boost/boost_1_84_0")
 set(Boost_USE_STATIC_LIBS ON)
-set(Boost_USE_DEBUG_LIBS ON)
-set(Boost_USE_MULTITHREADED ON)
-set(Boost_USE_STATIC_RUNTIME OFF)
+set(Boost_USE_MULTITHREADED OFF)
+set(Boost_USE_STATIC_RUNTIME ON)
 
-find_package(Boost 1.67.0 REQUIRED COMPONENTS 
-    program_options chrono filesystem system date_time)
+find_package(Boost 1.84.0 COMPONENTS chrono filesystem system)
 
-if (0)
-	message(STATUS "====> Boost_INCLUDE_DIRS: ${Boost_INCLUDE_DIRS}")
-	message(STATUS "====> Boost_LIBRARY_DIR_DEBUG: ${Boost_LIBRARY_DIR_DEBUG}")
-	message(STATUS "====> Boost_LIBRARIES: ${Boost_LIBRARIES}")
+if(Boost_FOUND)
+    include_directories(${Boost_INCLUDE_DIRS})
+    add_executable(progname file1.cxx file2.cxx)
+    target_link_libraries(progname ${Boost_LIBRARIES})
+else()
+	message("Failed to find boost")
+	message("looked in " ${BOOST_ROOT})
 endif()
-
-include_directories(${Boost_INCLUDE_DIRS})
-
